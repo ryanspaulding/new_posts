5f8a13760f3f12d0d3083f623283dca0c7202c3e
Ryan Spaulding <ryanspaulding@gmail.com>
2011-10-23 17:16:02 -0700

    have HTML displaying correctly and the date in the same format as the blog

--- new_posts.rb
+++ new_posts.rb
@@ -4,18 +4,45 @@ require 'erb'
 require 'rexml/document'
 require 'net/http'
 require 'uri'
+require 'date'
 
-if ARGV.length > 2
+# set this if you do not want to pass a URL on the commandline 
+URL = nil
+
+# little function to append 'st, nd, rd or th' after the day of the month:
+# October 10th, 2011
+def ordinalize(day)
+	if (11..13).include?(day % 100)
+      		return "#{day}th"
+    	else
+      		case day % 10
+        		when 1; return "#{day}st"
+        		when 2; return "#{day}nd"
+        		when 3; return "#{day}rd"
+        		else    return "#{day}th"
+      		end
+	end
+
+end
+
+
+if URL.nil? and ARGV.length > 2
 	puts "new_posts.rb takes one argument, the feed you want to get new posts from"
 	exit(1)
 end
 
-@posts_feed = ARGV[0]
+if URL.nil?
+	@posts_feed = ARGV[0]
+else
+	@posts_feed = URL
+end
+
 if ARGV[1].nil?
 	@limit = 3
 else
 	@limit = ARGV[1].to_i
 end
+
 @counter = 0
 @posts = []
 
@@ -31,7 +58,13 @@ begin
 		if @counter == @limit
 			break
 		end
-		@posts.push({:title => item.elements["title"].text, :link => item.elements["link"].text, :date => item.elements["pubDate"].text, :description => item.elements["description"].text})
+		# dates are stored in the following format => Mon, 10 Oct 2011 06:21:20 +0000
+		# and we want them to look like this => October 20th, 2011
+		@date_pieces = item.elements["pubDate"].text.split(" ")
+		@good_date = "#{@date_pieces[2]}/#{@date_pieces[1]}/#{@date_pieces[3]}"
+		# now the format is the format it is in not the format you want
+		@nice_date = Date.strptime(@good_date, "%b/%d/%Y")
+		@posts.push({:title => item.elements["title"].text, :link => item.elements["link"].text, :date => @nice_date.strftime("%B #{ordinalize(@nice_date.day)}, %Y"), :description => item.elements["description"].text})
 		@counter += 1
 	end
 
@@ -43,10 +76,10 @@ end
 
 html_for_posts = ERB.new <<EOF
 <% @posts.each do |post| %>
-<h2><a href="<%=puts post[:link]%>" rel="bookmark" title="Permanent Link to <%=post[:title]%>"><%=post[:title]%></a></h2>
+<h2><a href="<%=post[:link]%>" rel="bookmark" title="Permanent Link to <%=post[:title]%>"><%=post[:title]%></a></h2>
 				<small><%=post[:date]%></small>
 	<p>
-		<%=post[:description]%><a href="<%=post[:link]%>">Read more...</a>
+		<%=post[:description]%> <a href="<%=post[:link]%>">Read more...</a>
 	</p>
 <% end %>
 EOF
